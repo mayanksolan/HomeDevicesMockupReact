adc833845e81ef84432b214c6a718a0308a0c3bc
Mayank Solan <mayanksolan@gmail.com>
2019-12-20 08:59:05 +0530

    changes

--- client/src/components/menu/MenuList.js
+++ client/src/components/menu/MenuList.js
@@ -3,20 +3,67 @@ import menuArr from "./MenuListArr";
 
 export default class MenuList extends Component {
   render() {
+    const menulistMainSelect = {
+      margin: "50px 40px auto 107px"
+    };
+    const menulistMainNoSelect = {
+      margin: "50px 37px auto 140px"
+    };
+    const menulistTextStyleSelect = {
+      backgroundColor: "#ffffff",
+      width: "233px",
+      height: "140px"
+    };
+    const menulistTextStyleNoSelect = {
+      backgroundColor: "#ffffff",
+      width: "203px",
+      height: "120px"
+    };
+    const menulistImageSelect = {
+      position: "relative",
+      left: "170px",
+      bottom: "55px"
+    };
+    const menulistImageNoSelect = {
+      position: "relative",
+      right: "100px",
+      bottom: "50px"
+    };
     console.log(menuArr);
     return (
       <div>
         {menuArr.map(item => (
-          <div>
-            <div>
-              <img src={item.src} alt={item.name} key={item.id} />
-            </div>
-            <div className="item-text">
-              <div>{item.name}</div>
-              <div>
-                <span>In </span>
-                {item.where}
+          <div
+            key={item.id}
+            style={
+              item.name === "BED LAMP"
+                ? menulistMainSelect
+                : menulistMainNoSelect
+            }
+          >
+            <div
+              style={
+                item.name === "BED LAMP"
+                  ? menulistTextStyleSelect
+                  : menulistTextStyleNoSelect
+              }
+            >
+              <div className="menulist-text">
+                <div className="menulist-name">{item.name}</div>
+                <div className="menulist-where">
+                  <span>In </span>
+                  {item.where}
+                </div>
               </div>
+              <img
+                style={
+                  item.name === "BED LAMP"
+                    ? menulistImageSelect
+                    : menulistImageNoSelect
+                }
+                src={item.src}
+                alt={item.name}
+              />
             </div>
           </div>
         ))}
